5dec707509292a6e88dbbb0fbca015c059158834
Fengda HUANG <h@phodal.com>
2015-01-01 11:52:23 +0800

    Add post event

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,18 +1,23 @@
 define([
-
-], function() {
+    "jquery"
+], function ($) {
     chrome.extension.onRequest.addListener(
         function (request, sender, sendResponse) {
             console.log(sender.tab ?
             "from a content script:" + sender.tab.url :
                 "from the extension");
-            if (request.message_type == "story")
+            if (request.message_type == "story") {
+                console.log(request);
+                $.post("http://localhost:5000/story", request, function(data,status){
+                    console.log("Data: " + data + "\nStatus: " + status);
+                });
                 sendResponse({
                     status: 201
                 });
+            }
             else
                 sendResponse({
-                    status: 500
+                    status: 404
                 });
         });
 
--- app/scripts/lib/requirejs-config.js
+++ app/scripts/lib/requirejs-config.js
@@ -1,13 +1,8 @@
 require.config({
 	baseUrl: "scripts/",
 	paths: {
-		//underscore: "lib/underscore",
-		//URIjs: "lib/URI",
-		jquery: "scripts/lib/jquery.min"
-		//backbone: "lib/backbone",
-		//mustache: "lib/mustache",
-		//text: "lib/text",
-		//json: "lib/json"
+		underscore: "lib/underscore",
+		jquery: "lib/jquery.min"
 	},
 	shim: {
 		jquery: {
