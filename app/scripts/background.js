8079cc621956a8934ccde4a484617d2430784ab1
Fengda HUANG <h@phodal.com>
2015-01-01 11:37:34 +0800

    genearte simple jira story template

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -5,14 +5,18 @@ chrome.extension.onRequest.addListener(
         console.log(sender.tab ?
         "from a content script:" + sender.tab.url :
             "from the extension");
-        if (request.greeting == "hello")
-            sendResponse({farewell: "goodbye"});
+        if (request.message_type == "story")
+            sendResponse({
+                status: 201
+            });
         else
-            sendResponse({}); // snub them.
+            sendResponse({
+                status: 500
+            }); 
     });
 
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.browserAction.setBadgeText({text: 'Jira Crawler'});
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,6 +1,21 @@
 (function () {
     "use strict";
-    chrome.extension.sendRequest({greeting: "hello"}, function (response) {
-        console.log(response.farewell);
+	var message = {
+        user: "Phodal",
+        story: {
+            number: "001",
+            type: "bug",
+            title: "title",
+            day: 2,
+            description: "description"
+        },
+        message_type: "story"
+    };
+    chrome.extension.sendRequest(message, function (response) {
+        if(response.status === 201){
+            console.log("Success");
+        } else {
+            console.log("Failure");
+        }
     });
 }());
