4a907c7b5faa42fc7826395fbac311293ff44776
Fengda HUANG <h@phodal.com>
2015-01-04 09:23:29 +0800

    add info from jira

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,19 +1,29 @@
 (function () {
     "use strict";
-    var message = {
-        user: "Phodal",
-        story_number: "001",
-        story_type: "bug",
-        story_title: "title",
-        story_day: 2,
-        story_description: "description",
-        message_type: "story"
+
+    var getInfo = function (callback){
+        console.log("--------------------");
+        $(".ghx-first ul li:nth-child(3) .ghx-issue").each(
+            function (data) {
+                var results = [];
+                results["story_number"] = $(this).find(".ghx-key a").text();
+                results["story_description"] = $(this).find(".ghx-summary span").text();
+                results["user"] = $(this).find(".ghx-avatar img").attr("alt");
+                results["story_type"] = $(this).find(".ghx-type").attr("title");
+                results["story_day"] = $(this).find(".ghx-days").attr("title").match(/[^% days]/i)[0];
+                results["message_type"] = "story";
+                console.log(results);
+                callback(results);
+            });
     };
-    chrome.extension.sendRequest(message, function (response) {
-        if (response.status === 201) {
-            console.log("Success");
-        } else {
-            console.log("Failure");
-        }
+
+    getInfo(function(message){
+        chrome.extension.sendRequest(message, function (response) {
+            if (response.status === 201) {
+                console.log("Success");
+            } else {
+                console.log("Failure");
+            }
+        });
     });
 }());
